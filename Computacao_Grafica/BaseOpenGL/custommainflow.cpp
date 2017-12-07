a30b172523d644e0e6a09414afe7db0a75be1c3f
Lucas Salvino de Deus <lucassalvino1@gmail.com>
2017-12-06 22:51:52 -0200

    maquete

--- Computacao_Grafica/BaseOpenGL/custommainflow.cpp
+++ Computacao_Grafica/BaseOpenGL/custommainflow.cpp
@@ -2,38 +2,49 @@
 
 void CustomMainFlow::execute()
 {
-    Ponto a;
-    a.setColor(_DEFINECOLOR::DEFINECOLOR::RED);
-    defaultPrinObjeto(&a);
-    Ponto b(1,1,1);
-    b.setColor(_DEFINECOLOR::DEFINECOLOR::GREEN);
-    defaultPrinObjeto(&a);
-
+    using namespace _DEFINECOLOR;
+    //glTranslatef(0.0,ALTURA,0.0);
+    int tamanhoArena = 200; //tamanho da base do predio
+    int ladoPredio = 8; //cumprimento do lado do predio
+    int alturaPredio = 40; //altura do predio
+    int numeroAndares = 20; //numero de andares
+    float alturaAndar = (float)alturaPredio/(float)numeroAndares;
+    /*CHAO*/
     glPushMatrix();
-        glColor3f(1.0, 0.0, 0.0);
-        glBegin(GL_POLYGON);
-        glVertex2f(0.0, 0.0);
-        glVertex2f(0.0, 10.0);
-        glVertex2f(10.0, 10.0);
-        glVertex2f(10.0, 0.0);
-        glEnd();
+        DEFINECOLOR::BLACK();
+        glTranslatef(tamanhoArena/2*-1,0,tamanhoArena/2*-1);
+        glRotatef (0.0, 0.0, 0.0, 1.0);
+        Paralelo(0.25,tamanhoArena,tamanhoArena);
     glPopMatrix();
-
-    glPushMatrix();
-        glColor3f(0.0, 1.0, 0.0);
-        glBegin(GL_QUADS);
-        glVertex2f(10.0, 10.0);
-        glVertex2f(10.0, 20.0);
-        glVertex2f(20.0, 20.0);
-        glVertex2f(20.0, 10.0);
-        glEnd();
-    glPopMatrix();
-    glPushMatrix();
-        glColor3f(0.5, 0.5, 0.0);
-        glTranslatef(-15.0,10.0,0.0);
-        glRotatef (45.0, 0.0, 0.0, 1.0);
-        glutSolidCube(4);
-    glPopMatrix();
+    /*Colunas Principais*/
+    DEFINECOLOR::YELLOW();
+    PRINTCOLUNA(ladoPredio,ladoPredio, alturaPredio);
+    PRINTCOLUNA(-1*ladoPredio,ladoPredio, alturaPredio);
+    PRINTCOLUNA(ladoPredio,-1*ladoPredio, alturaPredio);
+    PRINTCOLUNA(-1*ladoPredio,-1*ladoPredio, alturaPredio);
+    /*ANDARES*/
+    float baseAndar = alturaAndar;
+    for(int i = 0; i<numeroAndares; i++, baseAndar+=alturaAndar){
+        DEFINECOLOR::GRAY();
+        glPushMatrix();
+                glTranslated(ladoPredio+0.5,0,ladoPredio);
+                Paralelo(baseAndar,(ladoPredio*-2),0.25);
+        glPopMatrix();
+        glPushMatrix();
+                glTranslated((ladoPredio+0.5),0,0.75+(-1*ladoPredio));
+                Paralelo(baseAndar,(ladoPredio*-2),0.25);
+        glPopMatrix();
+        glPushMatrix();
+                glTranslated(ladoPredio+0.5,0,ladoPredio);
+                glRotated(270,0,1,0);
+                Paralelo(baseAndar,(ladoPredio*-2),0.25);
+        glPopMatrix();
+        glPushMatrix();
+                glTranslated(-1*ladoPredio+0.5,0,0.75+ladoPredio);
+                glRotated(270,0,1,0);
+                Paralelo(baseAndar,(ladoPredio*-2),0.25);
+        glPopMatrix();
+    }
 }
 
 void CustomMainFlow::defaultPrinObjeto(TemplateObjeto *objeto)
@@ -43,4 +54,28 @@ void CustomMainFlow::defaultPrinObjeto(TemplateObjeto *objeto)
     objeto->printObjeto();
     glEnd();
     glPopMatrix();
+}
+
+void CustomMainFlow::Paralelo(float comp, float lar, float alt)
+{
+    glBegin(GL_QUADS);
+    glNormal3f(0,0,-1);
+    glVertex3f(00, 0.0, 0.0);  glVertex3f(lar, 0.0, 0.0);
+    glVertex3f(lar, comp, 0.0); glVertex3f(0.0, comp, 0.0);
+    glNormal3f(0,0,1);
+    glVertex3f(0.0, 0.0, alt);  glVertex3f(lar, 0.0, alt);
+    glVertex3f(lar, comp, alt); glVertex3f(0.0, comp, alt);
+    glNormal3f(-1,0,0);
+    glVertex3f(0.0, 0.0, 0.0);  glVertex3f(0.0, 0.0, alt);
+    glVertex3f(0.0, comp, alt); glVertex3f(0.0, comp, 0.0);
+    glNormal3f(0,-1,0);
+    glVertex3f(0.0, 0.0, 0.0);  glVertex3f(0.0, 0.0, alt);
+    glVertex3f(lar, 0.0, alt);  glVertex3f(lar, 0.0, 0.0);
+    glNormal3f(1,0,0);
+    glVertex3f(lar, 0.0, 0.0);  glVertex3f(lar, 0.0, alt);
+    glVertex3f(lar, comp, alt); glVertex3f(lar, comp, 0.0);
+    glNormal3f(0,1,0);
+    glVertex3f(0.0, comp, 0.0); glVertex3f(0.0, comp, alt);
+    glVertex3f(lar, comp, alt); glVertex3f(lar, comp, 0.0);
+    glEnd();
 }
--- Computacao_Grafica/BaseOpenGL/custommainflow.h
+++ Computacao_Grafica/BaseOpenGL/custommainflow.h
@@ -2,6 +2,7 @@
 #define CUSTOMMAINFLOW_H
 #include <GL/glut.h>
 #include "Objetos/ponto.h"
+#define PRINTCOLUNA(C1, C2, AL)glPushMatrix();glTranslatef(C1,0,C2);Paralelo(AL,1,1);glPopMatrix();
 using namespace std;
 class CustomMainFlow
 {
@@ -9,6 +10,7 @@ public:
     static void execute();
 private:
     static void defaultPrinObjeto(TemplateObjeto* objeto);
+    static void Paralelo(float comp, float lar, float alt);
 };
 
 #endif // CUSTOMMAINFLOW_H
