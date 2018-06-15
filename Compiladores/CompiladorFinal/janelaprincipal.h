49ddc5f1febe4ae9f1697b0aa93b290acd9f02fd
Lucas Salvino de Deus <lucassalvino1@gmail.com>
2018-06-15 02:03:54 -0300

    final

---
+++ Compiladores/CompiladorFinal/janelaprincipal.h
@@ -0,0 +1,27 @@
+#ifndef JANELAPRINCIPAL_H
+#define JANELAPRINCIPAL_H
+
+#include <QMainWindow>
+#include "logica.h"
+
+namespace Ui {
+class JanelaPrincipal;
+}
+
+class JanelaPrincipal : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit JanelaPrincipal(QWidget *parent = 0);
+    ~JanelaPrincipal();
+    Logica logica;
+
+private slots:
+    void on_actionCarregar_Tabela_triggered();
+
+private:
+    Ui::JanelaPrincipal *ui;
+};
+
+#endif // JANELAPRINCIPAL_H
